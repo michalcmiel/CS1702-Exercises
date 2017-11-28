547f54240d3429fc7bfb3770b7ded4c253aa233d
1707076 <1707076@brunel.ac.uk>
2017-11-28 16:52:52 +0000

    working on rule 234

--- src/uk/ac/brunel/cs1702/SudokuVerifier.java
+++ src/uk/ac/brunel/cs1702/SudokuVerifier.java
@@ -1,12 +1,55 @@
 package uk.ac.brunel.cs1702;
-
+import java.util.*;
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution){
+		if (candidateSolution.length()!=81){
+			return -5;
+		}
+		return 0;
+
+	}
+	
+	public int positiveInt(String candidateSolution){
+		for (int j=0; j<candidateSolution.length(); j++)
+		{
+			int val = candidateSolution.charAt(j);
+			if (val<0)
+			{
+				return -1;
+			}
+		}
 		return 0;
 	}
 	
+	public int subpositive (String candidateSolution){
+		int rows;
+		int cols;
+		for (int i = 0; i<9; i++)
+		{
+			int initial = i;
+			for(int j=0; j<9; j++){
+				System.out.print(initial);
+				initial +=9;
+			}
+		}
+			
+		return 0;
+		
+	}
+	
+	
 	public static void main(String[] args){
-
+		
+		String candidateSolution = "";
+		Random r = new Random();
+		for (int i=0; i<81; i++)
+		{
+			int n = r.nextInt(8)+1;
+			candidateSolution+=Integer.toString(n);
+		}
+		System.out.println(candidateSolution);
+		System.out.println(candidateSolution.length());
+		System.out.println(candidateSolution.charAt(0));
 	}
 }
