86570618d8f65bb9cf64aeaf3fe18e7f41af6272
1707076 <1707076@brunel.ac.uk>
2017-12-01 13:51:47 +0000

    working on rule 2 now

--- src/uk/ac/brunel/cs1702/SudokuVerifier.java
+++ src/uk/ac/brunel/cs1702/SudokuVerifier.java
@@ -1,93 +1,94 @@
 package uk.ac.brunel.cs1702;
 import java.util.*;
 public class SudokuVerifier {
-	
-	public int verify(String candidateSolution){
-		if (candidateSolution.length()!=81){
-			return -5;
-		}
-		return 0;
 
+	public static boolean checkLength(String candidateSolution){
+		if (candidateSolution.length()!=81){			//returns false if length is not equal to 81
+			return false;
+		}
+		return true;
 	}
-	
-	public int positiveInt(String candidateSolution){
-		for (int j=0; j<candidateSolution.length(); j++)
-		{
-			int value = candidateSolution.charAt(j);
-			if (value<0)
-			{
-				return -1;
+
+	public static boolean check1(String candidateSolution){
+		if (candidateSolution.matches("[1-9]*")){	//returns true if the numbers are from 1-9
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean check3(int[][] intArray){
+		for (int j=0; j<9 ; j++) {
+			int sum = 0;
+			for (int k=0; k<9; k++) {
+				sum = sum+intArray[j][k];
+			}
+			if (sum != 45){			
+				return false;			//returns false if the sum doesnt add up to 45
 			}
 		}
+		return true; 
+	}
+
+	public static boolean check4(int[][] intArray){
+		for (int j=0; j<9 ; j++) {
+			int sum = 0;
+			for (int k=0; k<9; k++) {
+				sum = sum+intArray[k][j];
+			}
+			if (sum != 45){
+				return false;
+			}
+		}
+		return true; 
+	}
+
+	public int verify(String candidateSolution){
+		if (checkLength(candidateSolution)==false){
+		return -5;
+		}
+		
+		if (check1(candidateSolution)==false){
+			return -1;
+		}
+		
+		int[][] intArray = new int[9][9];
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<9; j++) {
+				intArray[i][j] = Character.getNumericValue(candidateSolution.charAt(j+(i*9)));		//makes a 2d array from the string. i gets multiplied by 9 each time the loop runs
+		}
+		}
+		if (check3(intArray)==false){
+			return -3;
+		}
+		if (check4(intArray)==false){
+			return -4;
+		} 
 		return 0;
 	}
 	
-	public int Row(String candidateSolution){
-	{
-/*    	String candidateSolution = "011345678011345678012345678012345678012345678012345678012345678012345678012345678";*/
-    	    int[] intArray=new int[9];
-    	    
-    	    {
-    		for (int j=0; j<9; j++) {
-        		for (int i=0; i<9; i++) {
-    			intArray[j] = Integer.parseInt(String.valueOf(candidateSolution.charAt(j)));
-    		}
-    		}
-    	    
-    	System.out.println(Arrays.toString(intArray));
+	public static void main(String[] args) {
 
-/*		for (int j=0; j<intArray.length; j++) {			
-			for (int k=0; k<intArray.length; k++) {
-				if (k!=j && intArray[j] == intArray[k]) 		//checks if the numbers repeat{
-                System.out.println("Duplicate element found"); 
-        }
-        }*/
-        
-    }
-        System.out.println("Duplicate element not found"); 
-    }
-	return 0;
+		String candidateSolution = "123456789123456789123456789123456789123456789123456789123456789123456789123456789";
+		int[][] intArray = new int[9][9];
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<9; j++) {
+				intArray[i][j] = Character.getNumericValue(candidateSolution.charAt(j+(i*9)));
+			}
+		}
+			{
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<9; j++) {
+				
+				System.out.print(intArray[i][j]+" ");
+			}
+			System.out.println();
+		}
+		if (check1(candidateSolution)==false){
+			System.out.println("rule 1 ");
+		}else System.out.println("good");
+			}
+	}
 }
 
-	
-	
-	
-	
-	
-/*	public int Row(String candidateSolution) {
-		int[] intArray = new int[9];
-		for (int i=0; i<9; i++) {
-			intArray[i] = Integer.parseInt(String.valueOf(candidateSolution.charAt(i))); // converts string to int array
-		}
-		for (int j=0; j<intArray.length; j++) {
-			for (int k=0; k<intArray.length; k++) {
-				if (k!=j && intArray[j] == intArray[k]) {
-					return -3;
-				}
-			}
-		}
-		return 0;
-	}
-	*/
-	
-		
-		
 
-	
-	public static void main(String[] args){
-		
-		String candidateSolution = "";
-		Random r = new Random();
-		for (int i=0; i<81; i++)
-		{
-			int n = r.nextInt(8)+1;
-			candidateSolution+=Integer.toString(n);
-		}
-		System.out.println(candidateSolution);
-		System.out.println(candidateSolution.length());
-		System.out.println(candidateSolution.charAt(0));
-/*		System.out.println(row(candidateSolution));*/
-		}
-		
-	}
 
