be698ce642604632be592aa3926a4079c83ec942
1707076 <1707076@brunel.ac.uk>
2017-12-05 17:13:44 +0000

    figuring out the methods

--- src/uk/ac/brunel/cs1702/MyStore.java
+++ src/uk/ac/brunel/cs1702/MyStore.java
@@ -20,16 +20,18 @@ public class MyStore {
        
         MyStore myStore = new MyStore();
         myStore.items.add(new ItemForSale("Herbal Health Supplements", 10, 5));
-        myStore.items.add(new ItemForSale("Aged Parmesan cheese", 20, 10));
+        myStore.items.add(new ItemForSale("Aged Parmesan cheese", 20, 10));	//
         myStore.items.add(new ItemForSale("Local soda drink", 20, 3));
         myStore.items.add(new ItemForSale("World War I Medals", 0, 80));
-        myStore.items.add(new ItemForSale("Tickets to Iron Maiden concert", 15, 20));
+        myStore.items.add(new ItemForSale("Tickets to Iron Maiden concert", 15, 20));	//
         myStore.items.add(new ItemForSale("Special cake baked in store", 3, 6));
 
         myStore.updateInventory();
        
         System.out.println("Inventory Updated!");
     }
+	public void chees(){
+	}
 	
     public void updateInventory() {
         for (int i = 0; i < items.size(); i++) {
@@ -38,12 +40,13 @@ public class MyStore {
                 if (items.get(i).getPrice() > 0) {
                     if (!items.get(i).getName().equals("World War I Medals")) {
                         items.get(i).setPrice(items.get(i).getPrice() - 1);
+                        
                     }
                 }
             } else {
                 if (items.get(i).getPrice() < 50) {
                     items.get(i).setPrice(items.get(i).getPrice() + 1);
-
+                    
                     if (items.get(i).getName().equals("Tickets to Iron Maiden concert")) {
                         if (items.get(i).getNumberOfDaysToSellIn() < 11) {
                             if (items.get(i).getPrice() < 50) {
@@ -51,6 +54,12 @@ public class MyStore {
                             }
                         }
 
+                        
+                        
+                        
+                        
+                        
+                        
                         if (items.get(i).getNumberOfDaysToSellIn() < 6) {
                             if (items.get(i).getPrice() < 50) {
                                 items.get(i).setPrice(items.get(i).getPrice() + 1);
@@ -83,5 +92,25 @@ public class MyStore {
             }
         }
     }
+    
+    public void cheese(int i){
+    	if(items.get(i).getName().equals("Aged Parmesan cheese")){
+    		if (items.get(i).getPrice() < 50) {
+    			items.get(i).setPrice(items.get(i).getPrice() +1);
+    		}
+    	}
+    }
+    
+    
+    
+    public void cake(int i){
+    	if(items.get(i).getName().equals("Special cake baked in store")){
+    		if (items.get(i).getPrice() < 50) {
+    			items.get(i).setPrice(items.get(i).getPrice() -2);
+    		}
+    	}
+    }
+    
+    
 }
 
